30c8582d24d5e67bff31de5b0f3b33af24af841d
Nilupul Sandeepa <nilupuls01@gmail.com>
2017-05-01 08:54:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ app.post('/upload', function(req, res){
 
 });
 
-app.listen(3000, function(){
-    console.log('Server listening on port 3000');
+app.listen(5000, function(){
+    console.log('Server listening on port 5000');
 });
